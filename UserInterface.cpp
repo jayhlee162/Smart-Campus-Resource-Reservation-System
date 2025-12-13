d49b6085b0077e9e57cd987d321d0eb8f6d96d28
jackdeakins <jdeakins1998@gmail.com>
2025-12-13 01:53:46 -0800

    add UI menus, add main function body

--- UserInterface.cpp
+++ UserInterface.cpp
@@ -1,1 +1,29 @@
+#include "UserInterface.h"
 
+void UserInterface::printAdminMenu(){
+    std::cout << std::string(54, '-') + '\n'
+              << "| Welcome to the Campus Resource Reservation System! |\n"
+              << std::string(54, '-') + '\n'
+              << "\n"
+              << "Please select an option: \n"
+              << "1. Create Reservation\n"
+              << "2. Cancel Reservation\n"
+              << "3. View Current Reservations\n"
+              << "4. List Available Resources\n"
+              << "5. Add Resource\n"
+              << "6. Remove Resource\n"
+              << "7. Edit resource\n"
+              << "8. Exit\n";
+}
+void UserInterface::printStudentMenu(){
+    std::cout << std::string(54, '-') + '\n'
+              << "| Welcome to the Campus Resource Reservation System! |\n"
+              << std::string(54, '-') + '\n'
+              << "\n"
+              << "Please select an option: \n"
+              << "1. Create Reservation\n"
+              << "2. Cancel Reservation\n"
+              << "3. View Current Reservations\n"
+              << "4. List Available Resources\n"
+              << "5. Exit\n";
+}
--- main.cpp
+++ main.cpp
@@ -1,3 +1,71 @@
+#include "UserInterface.h"
+#include "School.h"
 int main(){
+  UserInterface run;
+  User u1;
+  School s1;
+  int input;
+  bool endProgram = false;
+  //code to handle login
+
+  if(u1.isAdmin())
+  do{
+    run.printAdminMenu();
+    std::cin >> input;
+    
+    switch (input)
+    {
+    case 1:
+      s1.createReservation(u1);
+      break;
+    case 2:
+      s1.cancelReservation(u1);
+      break;
+    case 3:
+      s1.printReservation(u1);
+      break;
+    case 4:
+      s1.printResources();
+      break;
+    case 5:
+      s1.addResource();
+      break;
+    case 6:
+      s1.removeResource();
+      break;
+    case 7:
+      s1.editResource();
+      break;
+    case 8:
+      endProgram = true;
+      break;
+    default:
+      break;
+    }
+  }while (!endProgram);
+
+  else
+  do{
+    run.printStudentMenu();
+    std::cin >> input;
+    switch (input)
+    {
+    case 1:
+      s1.createReservation(u1);
+      break;
+    case 2:
+      s1.cancelReservation(u1);
+      break;
+    case 3:
+      s1.printReservation(u1);
+      break;
+    case 4:
+      s1.printResources();
+      break;
+    case 5:
+      endProgram = true;
+      break;
+    }
+  }while (!endProgram);
   return 0;
 }
