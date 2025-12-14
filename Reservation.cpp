c44e505f2dfdf33ae7c6eebfd40b9b30ead642f3
Jaehun <jayhlee162@gmail.com>
2025-12-14 02:54:23 -0800

    Integrated reservation logic .cpp and .h. Need other people's code to continue

--- Reservation.cpp
+++ Reservation.cpp
@@ -1,1 +1,50 @@
+#include "Reservation.h"
+#include <iostream>
 
+Reservation::Reservation() : resourceId(-1), timeSlot(-1), dayIndex(-1) {}
+Reservation::Reservation(int id, int t, int d) : resourceId(id), timeSlot(t), dayIndex(d) {}
+
+int Reservation::getResourceId()
+{
+    return resourceId;
+}
+int Reservation::getTimeSlot()
+{
+    return timeSlot;
+}
+int Reservation::getDayIndex()
+{
+    return dayIndex;
+}
+
+Reservation &Reservation::setResourceId(int id)
+{
+    resourceId = id;
+    return *this;
+}
+Reservation &Reservation::setTimeSlot(int t)
+{
+    timeSlot = t;
+    return *this;
+}
+Reservation &Reservation::setDayIndex(int d)
+{
+    dayIndex = d;
+    return *this;
+}
+
+bool Reservation::collisionCheck(const Reservation &other) const
+{
+    {
+        if (resourceId != other.resourceId)
+            return false;
+        if (dayIndex != other.dayIndex)
+            return false;
+        if (timeSlot != other.timeSlot)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
--- Reservation.h
+++ Reservation.h
@@ -1,1 +1,26 @@
+#pragma once
+#include <string>
 
+class Reservation
+{
+private:
+    // School sch;
+    int resourceId;
+    int timeSlot;
+    int dayIndex;
+
+public:
+    Reservation() : resourceId(-1), timeSlot(-1), dayIndex(-1) {}
+    Reservation(int id, int t, int d) : resourceId(id), timeSlot(t), dayIndex(d) {}
+
+    int getResourceId();
+    int getTimeSlot();
+    int getDayIndex();
+
+    Reservation &setResourceId(int id);
+    Reservation &setTimeSlot(int t);
+    Reservation &setDayIndex(int d);
+
+    // collision resolution
+    bool collisionCheck(const Reservation &other) const;
+};
